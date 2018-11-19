3ac1c04a2fdbb7d0f3b607bba6ddb38c57b1374b
injecti0n <rootgebruiker@gmail.com>
2018-11-19 00:55:25 -1000

    shpinx documentation generated

---
+++ card.py
@@ -0,0 +1,72 @@
+import random
+
+class Card:
+    def helloworld():
+        """
+        This function prints 'hello world' to the screen.
+        It accepts no arguments and returns nothing.
+        """
+        print("hello world")
+        
+    # the constructor for a class.
+    def __init__(self, suit, number):
+        self._suit = suit
+        self._number = number
+        
+    # RePresenation of number and suit
+    def __repr__(self):
+        return self._number + " of " + self._suit
+
+    @property
+    def suit(self):
+        return self._suit
+    
+    # Searching the suit in the Array and then prints a suit(s) that user ask.
+    @suit.setter
+    def suit(self, suit):
+        if suit in ["hearts", "clubs", "diamonds", "spades"]:
+            self._suit = suit
+        else:
+            print("That's not a suit!")
+
+    @property
+    def number(self):
+        return self._number
+
+
+    @number.setter
+    def number(self, number):
+        valid = [str(n) for n in range(2,11)] + ["J", "Q", "K", "A"]
+        if number in valid:
+            self._number = number
+        else:
+            print("That's not a valid number")
+
+
+class Deck:
+    # the constructor for a class.
+    def __init__(self):
+        self._cards = []
+        self.populate()
+    # This method shows a betting
+    def populate(self):
+        suits = ["hearts", "clubs", "diamonds", "spades"]
+        numbers = [str(n) for n in range(2,11)] + ["J", "Q", "K", "A"]
+        self._cards = [ Card(s, n) for s in suits for n in numbers ]
+
+    def shuffle(self):
+        random.shuffle(self._cards)
+
+    def deal(self, no_of_cards):
+        dealt_cards = []
+        for i in range(no_of_cards):
+            dealt_card = self._cards.pop(0)
+            dealt_cards.append(dealt_card)
+        return dealt_cards
+
+    def __repr__(self):
+        cards_in_deck = len(self._cards)
+        return "Deck of " + str(cards_in_deck) + " cards"
+        
+deck = Deck()
+print(deck)
